a03b7bfdb0c3726ae7586e008c5291503c7904cd
anish <shobhashama9@gmail.com>
2022-11-22 21:58:44 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,8 +106,8 @@ const Footer = () => {
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
-            Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Yashita Namdeo
+            Designed &amp; Built by Anish Sharma<br></br>
+            
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
