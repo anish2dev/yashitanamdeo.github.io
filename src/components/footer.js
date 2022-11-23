7180eaee94fa62ad6247f92f7904facd945fb7da
anish <shobhashama9@gmail.com>
2022-11-23 18:31:33 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,6 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
@@ -110,19 +109,7 @@ const Footer = () => {
             
           </div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <StyledGitHubInfo>
-              <span>
-                <FormattedIcon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <FormattedIcon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </StyledGitHubInfo>
-          )}
-        </StyledGitHubLink>
+          </StyledGitHubLink>
       </StyledMetadata>
     </StyledContainer>
   );
